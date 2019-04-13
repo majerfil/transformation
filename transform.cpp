039bce5da1363431555c34bc7b11d49beaf23699
Tom Krajnik <tomkrajnik@hotmail.com>
2019-04-13 09:32:49 +0200

    OpenCV transform

--- transform.cpp
+++ transform.cpp
@@ -17,38 +17,52 @@ vector<float> refX,refY,x,y,hom;
 vector<float> transform(vector<float> xi, vector<float> yi,vector<float> x,vector<float> y)
 {
 	printf("%i\n",xi.size());
-	Mat A(2*xi.size(),8,CV_32FC1);
-	Mat b(2*xi.size(),1,CV_32FC1);
-	Mat xx;
+	Mat A(xi.size(),4,CV_32FC1);
+	Mat b(xi.size(),1,CV_32FC1);
+	Mat T(2,3,CV_32FC1);
+
+	Mat xx(3,xi.size(),CV_32FC1);
+	Mat rx(2,xi.size(),CV_32FC1);
 
 	for (int i = 0;i<xi.size();i++){
-		A.at<float>(2*i,0)=-x[i];
-		A.at<float>(2*i,1)=-y[i];
-		A.at<float>(2*i,2)=1;
-		A.at<float>(2*i,3)=0;
-		A.at<float>(2*i,4)=0;
-		A.at<float>(2*i,5)=0;
-		A.at<float>(2*i,6)=-xi[i]*x[i];
-		A.at<float>(2*i,7)=-xi[i]*y[i];
-		A.at<float>(2*i+1,0)=0;
-		A.at<float>(2*i+1,1)=0;
-		A.at<float>(2*i+1,2)=0;
-		A.at<float>(2*i+1,3)=-x[i];
-		A.at<float>(2*i+1,4)=-y[i];
-		A.at<float>(2*i+1,5)=1;
-		A.at<float>(2*i+1,6)=-yi[i]*x[i];
-		A.at<float>(2*i+1,7)=-yi[i]*y[i];
-
-		b.at<float>(2*i,0)=-xi[i];
-		b.at<float>(2*i+1,0)=-yi[i];
+		A.at<float>(i,0)=yi[i]*x[i]-xi[i]*y[i];
+		A.at<float>(i,1)=yi[i]*y[i]+xi[i]*x[i];
+		A.at<float>(i,2)=yi[i];
+		A.at<float>(i,3)=-xi[i];
+		xx.at<float>(0,i)=x[i];
+		xx.at<float>(1,i)=y[i];
+		xx.at<float>(2,i)=1;
+		rx.at<float>(0,i)=xi[i];
+		rx.at<float>(1,i)=yi[i];
 	}
-	cout << A << endl;
-	solve(A,b,xx,DECOMP_SVD);
-	cout << xx << endl;
-	//MATINV(8,1,est,vec,&det);
-	//for (int i = 0;i<8;i++)  hom.push_back(vec[i][0]);
-	//hom.push_back(1);
-	//transformType = TRANSFORM_2D;*/
+	A = (A.t()*A);
+	cv::SVD svdMat(A);
+	cout << svdMat.u << endl;
+	cout << svdMat.w << endl;
+	cout << svdMat.vt << endl;
+	cv::SVD::solveZ(A,b);
+	if (b.at<float>(0,0) < 0) b=-b;
+	float a0 = b.at<float>(0,0);
+	float a1 = b.at<float>(1,0);
+	b = b/sqrt(a0*a0+a1*a1);
+	a0 = b.at<float>(0,0);
+	a1 = b.at<float>(1,0);
+	float tx = b.at<float>(2,0);
+	float ty = b.at<float>(3,0);
+	T.at<float>(0,0) = a0; 
+	T.at<float>(0,1) = a1; 
+	T.at<float>(1,0) = -a1; 
+	T.at<float>(1,1) = a0; 
+	T.at<float>(0,2) = tx; 
+	T.at<float>(1,2) = ty; 
+	cout << T << endl;
+ 	Mat e = T*xx-rx;
+	float err = 0;
+	for (int i = 0;i<xi.size();i++)
+	{
+		err += sqrt(e.at<float>(0,i)*e.at<float>(0,i)+e.at<float>(1,i)*e.at<float>(1,i));
+	}
+	printf("%f\n",err/xi.size());
 	return hom;
 }
 
@@ -62,6 +76,7 @@ int main(int argc,char* argv[])
 
 	ifstream inReferenceFile(argv[1]);
 	ifstream inFile(argv[2]);
+	ifstream outFile(argv[3]);
 	float a,b;
 
 	while(inReferenceFile >> a >> b)
@@ -72,7 +87,7 @@ int main(int argc,char* argv[])
 	}
 	while(inFile >> a >> b)
 	{
-		x.push_back(a);
+		x.push_back(a+atof(argv[4]));
 		y.push_back(b);
 	}
 	hom = transform(refX,refY,x,y);
